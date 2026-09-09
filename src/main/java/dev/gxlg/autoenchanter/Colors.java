25feb700fd405dc2089f0b32847d8e55a20ca1be
trongtdfhg30026-oss <trongtdfhg30026@gmail.com>
2026-09-09 15:02:13 +0700

    Delete src/main directory

--- src/main/java/dev/gxlg/autoenchanter/Colors.java
+++
@@ -1,8 +0,0 @@
-package dev.gxlg.autoenchanter;
-
-public class Colors {
-    public static final int RED = 0xFFC91414;
-    public static final int GREEN = 0xFFB7E371;
-    public static final int BLUE = 0xFF879EC9;
-    public static final int YELLOW = 0xFFFDFF75;
-}
