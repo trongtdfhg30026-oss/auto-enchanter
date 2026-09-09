e21bc6599b28968dcec374b321b50f2e40d18358
trongtdfhg30026-oss <trongtdfhg30026@gmail.com>
2026-09-10 06:44:21 +0700

    Add files via upload

---
+++ src/main/java/dev/gxlg/autoenchanter/Reflection.java
@@ -0,0 +1,7 @@
+package dev.gxlg.autoenchanter;
+
+@SuppressWarnings("unused")
+public class Reflection {
+    public static Object wrap(String i) { return new Object(); }
+    public static boolean version(String i) { return true; }
+}
