a3e0f9771e71b95e17cab10375f42f88a8221a04
trongtdfhg30026-oss <trongtdfhg30026@gmail.com>
2026-09-10 11:49:49 +0700

    Delete src/main directory

--- src/main/java/dev/gxlg/autoenchanter/Reflection.java
+++
@@ -1,7 +0,0 @@
-package dev.gxlg.autoenchanter;
-
-@SuppressWarnings("unused")
-public class Reflection {
-    public static Object wrap(String i) { return new Object(); }
-    public static boolean version(String i) { return true; }
-}
